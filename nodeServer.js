619c58f48c340c13ad8acbd2849c62bfdaf1e81f
merlin song <merlinsong@merlindeMac-mini.local>
2018-11-23 16:53:29 +0800

    s

--- nodeServer.js
+++ nodeServer.js
@@ -4,7 +4,7 @@ const port = 3009;
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World123\n');
+	res.end('Hello World1234\n');
 });
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
