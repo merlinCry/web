d70805b3e973079e47c52541268dd04ad72b806b
merlinCry <370007250@qq.com>
2018-11-23 15:39:52 +0800

    Update nodeServer.js
    端口更换为3309

--- nodeServer.js
+++ nodeServer.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = '0.0.0.0';
-const port = 3008;
+const port = 3009;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
